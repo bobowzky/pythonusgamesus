41d2386b6419c09dfc6c8b4656d21d98658218f2
ComradeBobo <robinhrazim@seznam.cz>
2023-01-20 19:36:15 +0100

    Save03

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ def main():
 
     while running:
         time+=timespeed
-        timespeed+=0.0001
+        timespeed+=0.001
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
             if event.type == pg.KEYDOWN:
-                if event.key == pg.K_d:
+                if event.key == pg.K_RIGHT:
                     auto.pohyb("do_prava")
-                if event.key == pg.K_a:
+                if event.key == pg.K_LEFT:
                     auto.pohyb("do_leva")
 
 
@@ -38,7 +38,6 @@ def main():
             auto.draw()
             enemy.pohyb(timespeed)
             enemy.draw()
-            # Červená barva pro skóre, je zde z důvodu, aby byl Matěj naštván (Na žádost Dana)
             text = font.render('Skóre: '+ str(body), True, (255,0,0))
             okenko.blit(text, (0,0,50,50))
             if enemy.passed():
@@ -48,10 +47,24 @@ def main():
                 game = False
         else:
             okenko.fill((0, 0, 0))
-            text = font.render('GAME OVER TVÉ SKÓRE JE: ' + str(body), True, (255, 0, 0))
+            text = font_velky.render('Konec hry', True, (255, 0, 0))
+            text2 = font_velky.render('Skóre: ' + str(body), True, (255, 0, 0))
+            text3 = font_velky.render('Pro pokračováni stiskni: R', True, (255,0,0))
             textRect = text.get_rect()
-            textRect.center = (450, 450)
+            textRect.center = (450, 400)
+            text2Rect = text.get_rect()
+            text2Rect.center = (470, 450)
+            text3Rect = text.get_rect()
+            text3Rect.center = (255, 500)
             okenko.blit(text, textRect)
+            okenko.blit(text2, text2Rect)
+            okenko.blit(text3, text3Rect)
+            if pg.key.get_pressed()[pg.K_r]:
+                game = True
+                time = 0
+                enemy = enemak.Enemka(okenko)
+                body = 0
+                timespeed = 1
 
         pg.display.flip()
         clock.tick(60)
